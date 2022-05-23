512ff044cf6bd8ad1c2aff2f62f03ba92195fe47
Alberto <alberto@nada.com>
2022-05-23 17:53:55 +0200

    m1

---
+++ js/ejer6.js
@@ -0,0 +1,11 @@
+
+//Pedir dos umeros y sumaros por pantalla
+let a=prompt('Primer numero');
+let b=prompt('segundo numero');
+//Convertir cadena a numero
+a=Number(a);
+b=Number(b);
+console.log(a+b);
+a=String(a);
+b=String(b);
+console.log(a+b);
